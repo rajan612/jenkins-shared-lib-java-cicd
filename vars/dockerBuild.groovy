508f5e81c67c6c84a5176e86b79a05de8d9f3526
rajan612 <rajan612@gmail.com>
2024-01-21 23:43:49 -0500

    updated docker function

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,6 @@ def call(String project, String hubUser, String imageTag){
     sh """
         docker image build -t ${hubUser}:${project} .
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker image tag ${hubUser}/${project} ${project}:latest
        """
 }
