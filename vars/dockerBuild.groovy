539e09ee6f37eeba26bfaf4e772f693fc51d3244
rajan612 <rajan612@gmail.com>
2024-01-21 23:29:17 -0500

    typo correction in the groovy anf Dockerfile

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,7 @@
 def call(String project, String hubUser, String imageTag){
     sh """
         docker image build -t ${hubUser}:${project} .
-        docker image tag ${hubUser}/${project} $hubUser}/${project}:${imageTag}
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
        """
 }
