f30142203e69917cf8e706d2aed761293e0a07a4
rvashisht <rvashisht@vibrenthealth.com>
2024-01-25 15:08:07 -0500

    updated the params sequence

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,6 +2,6 @@ def call(String project, String imageTag, String hubUser){
     sh """
         docker image build -t ${hubUser}:${project} .
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-        docker image tag ${hubUser}/${project} ${project}:latest
+        docker image tag ${hubUser}/${project} ${hubuser}/${project}:latest
        """
 }
